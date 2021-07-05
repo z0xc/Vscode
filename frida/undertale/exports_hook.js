277083ff836399932914c0294a93122d89337650
z0xc <zhouxinchi1998@foxmail.com>
2021-07-05 15:39:51 +0800

    Update exports_hook.js

--- frida/undertale/exports_hook.js
+++ frida/undertale/exports_hook.js
@@ -8,17 +8,10 @@ function exportshook(){
         if(funAddr != null){
                 Interceptor.attach(funAddr, {
                         onEnter: function(args){
-<<<<<<< Updated upstream
                                 if(args[0] == 0xD0C08260 || args[1] == 0x99A371E0){
                                     console.log("args0",args[0]);
                                     console.log("args1",args[1]);
                                     console.log("args2",args[2].toInt32());
-=======
-                                if(args[0] == 0x995691E0 || args[1] == 0x995691E0){
-                                    console.log(args[0]);
-                                    console.log(args[1]);
-                                    console.log(args[2].toInt32());
->>>>>>> Stashed changes
                                     // console.log(args[3].toInt32());
                                     // console.log(args[4].toInt32());
                         }
