8a778e284f49751dd212bb6d9b07998f6b6a0073
z0xc <zhouxinchi1998@foxmail.com>
2021-07-06 22:24:26 +0800

    Update exports_hook.js

--- frida/undertale/exports_hook.js
+++ frida/undertale/exports_hook.js
@@ -3,20 +3,21 @@ function hook1(){
         console.log("BaseAddress:",soAddr);
         // GET_RValue		
         // var funAddr = Module.findExportByName("libyoyo.so", "_Z10GET_RValueP6RValueS0_i");
-        var funAddr = Module.findExportByName("libyoyo.so", "_Z10SET_RValueP6RValueS0_i");
-        console.log("setfunAddr:",funAddr);
-        if(funAddr != null){
-                Interceptor.attach(funAddr, {
+        var setvalAddr = Module.findExportByName("libyoyo.so", "_Z10SET_RValueP6RValueS0_i");
+        // var globalAddr = Module.findExportByName("libyoyo.so", "_Z22Variable_Global_SetVariiP6RValue");
+        console.log("setvalAddr:",setvalAddr);
+        if(setvalAddr != null){
+                Interceptor.attach(setvalAddr, {
                         onEnter: function(args){
-                                if(args[0] == 0x995621E0 || args[1] == 0x995621E0){
+                                if(args[0] == 0xD09B5EA0 || args[1] == 0xD09B5EA0){
                                         console.log("args0",args[0]);
-                                        // var p1 = ptr(args[0]);
-                                        // console.log("args0 值",p1.readDouble());
+                                        var p1 = ptr(args[0]);
+                                        console.log("args0 值",p1.readDouble());
 
                                     
                                         console.log("args1",args[1]);
-                                        // var p2 = ptr(args[1]);
-                                        // console.log("args1 值",p2.readDouble());
+                                        var p2 = ptr(args[1]);
+                                        console.log("args1 值",p2.readDouble());
 
                                         console.log("args2",args[2]);
                                         var p3 = ptr(args[2]);
@@ -28,11 +29,12 @@ function hook1(){
 
                                         // console.log(args[3].toInt32());
                                         // console.log(args[4].toInt32());
+
                                 }
                         
                         },
                         onLeave: function(retval){
-                                // if(retval == 0x995621E0 ){
+                                // if(retval == 0xD09B5EA0 ){
                                 //         console.log("retval:",retval);
                                 //         var p4 = ptr(retval);
                                 //         console.log("retval 值",p4.readDouble());
@@ -46,26 +48,24 @@ function hook1(){
 function hook2(){
         // var soAddr = Module.findBaseAddress("libyoyo.so");
         // console.log("BaseAddress:",soAddr);
-        // GET_RValue		
-        // var funAddr = Module.findExportByName("libyoyo.so", "_Z10GET_RValueP6RValueS0_i");
         var funAddr = Module.findExportByName("libyoyo.so", "_Z22Variable_Global_SetVariiP6RValue");
         console.log("globalfunAddr:",funAddr);
         if(funAddr != null){
                 Interceptor.attach(funAddr, {
                         onEnter: function(args){
-                                // if(args[0] == 0x995621E0 || args[1] == 0x995621E0){
+                                // if(args[0] == 0xD09B5EA0 || args[1] == 0xD09B5EA0 || args[2] == 0xD09B5EA0){
                                         console. log(" global args0",args[0]);
-                                        // var p1 = ptr(args[0]);
+                                        var p1 = ptr(args[0]);
                                         // console.log("args0 值",p1.readDouble());
 
                                     
                                         console.log("global args1",args[1]);
-                                        // var p2 = ptr(args[1]);
-                                        // console.log("args1 值",p2.readDouble());
+                                        var p2 = ptr(args[1]);
+                                        // console.log("args1 值",p2.readInt());
 
                                         console.log("global args2",args[2]);
-                                        // var p3 = ptr(args[2]);
-                                        // console.log("args2 值",p3.readInt());
+                                        var p3 = ptr(args[2]);
+                                        console.log("args2 值",p3.readDouble());
                                         console.log("====================================================================------------------------------------");
                                         console.warn(Thread.backtrace(this.context, Backtracer.ACCURATE).map(DebugSymbol.fromAddress).join('\\n') + '\\n');
                                         console.warn(JSON.stringify(this.context));
@@ -77,7 +77,7 @@ function hook2(){
                         
                         },
                         onLeave: function(retval){
-                                // if(retval == 0x995621E0 ){
+                                // if(retval == 0xD09B5EA0 ){
                                 //         console.log("retval:",retval);
                                 //         var p4 = ptr(retval);
                                 //         console.log("retval 值",p4.readDouble());
@@ -89,8 +89,8 @@ function hook2(){
 
 function main(){
        	console.log("main-------begin");
-	// hook1();
-        hook2();
+	hook1();
+        // hook2();
 	console.log("main------end");
 
 }
