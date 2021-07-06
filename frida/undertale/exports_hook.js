2c74cb8115766957d44b564ba8384ab8b79edaf3
z0xc <zhouxinchi1998@foxmail.com>
2021-07-06 09:45:27 +0800

    Update exports_hook.js

--- frida/undertale/exports_hook.js
+++ frida/undertale/exports_hook.js
@@ -1,6 +1,6 @@
 function exportshook(){
-        // var soAddr = Module.findBaseAddress("libyoyo.so");
-        // console.log(soAddr);
+        var soAddr = Module.findBaseAddress("libyoyo.so");
+        console.log("BaseAddress:",soAddr);
         // GET_RValue		
         // var funAddr = Module.findExportByName("libyoyo.so", "_Z10GET_RValueP6RValueS0_i");
         var funAddr = Module.findExportByName("libyoyo.so", "_Z10SET_RValueP6RValueS0_i");
@@ -8,7 +8,7 @@ function exportshook(){
         if(funAddr != null){
                 Interceptor.attach(funAddr, {
                         onEnter: function(args){
-                                if(args[0] == 0x9DC031E0 || args[1] == 0x9DC031E0){
+                                if(args[0] == 0x995621E0 || args[1] == 0x995621E0){
                                         console.log("args0",args[0]);
                                         var p1 = ptr(args[0]);
                                         console.log("args0 值",p1.readDouble());
@@ -22,14 +22,17 @@ function exportshook(){
                                         var p3 = ptr(args[2]);
                                         console.log("args2 值",p3.readInt());
                                         console.log("====================================================================------------------------------------");
-                                        p2.writeDouble(p1.readDouble());  //锁血
+                                        console.warn(Thread.backtrace(this.context, Backtracer.ACCURATE).map(DebugSymbol.fromAddress).join('\\n') + '\\n');
+                                        console.warn(JSON.stringify(this.context));
+                                        // p2.writeDouble(p1.readDouble());  //锁血
+
                                         // console.log(args[3].toInt32());
                                         // console.log(args[4].toInt32());
                                 }
                         
                         },
                         onLeave: function(retval){
-                                // if(retval == 0x9DC031E0 ){
+                                // if(retval == 0x995621E0 ){
                                 //         console.log("retval:",retval);
                                 //         var p4 = ptr(retval);
                                 //         console.log("retval 值",p4.readDouble());
