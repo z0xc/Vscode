b50a2cb2707334a3b98519d21b39a74350f7d06d
unknown <zhouxc1@5fun.sh>
2021-07-13 14:09:13 +0800

    2021年7月13日

--- frida/undertale/exports_hook.js
+++ frida/undertale/exports_hook.js
@@ -9,7 +9,15 @@ function hook1(){
         if(setvalAddr != null){
                 Interceptor.attach(setvalAddr, {
                         onEnter: function(args){
-                                if(args[0] == 0xBEC83520){
+                                if(args[0] == 0xbd43d8a0){
+                                        var a1 = args[0];
+                                        var a2 = args[1];
+                                        console.log("a2:",a2);   //0xc1adf2ac
+                                        var temp = ptr(a2);
+                                        console.warn(hexdump(temp));
+                                        var a = ptr(0xc1adf2b8);
+                                        console.log("*a:",a.readPointer());
+                                        console.warn(hexdump(a));
                                         console.error("_Z10SET_RValueP6RValueS0_i");
                                         console.error("args0:",args[0]);
                                         var p1 = ptr(args[0]);
@@ -21,11 +29,9 @@ function hook1(){
                                         console.error("args1: 值",p2.readDouble());
 
                                         console.error("args2:",args[2]);
-                                        // var p3 = ptr(args[2]);
-                                        // console.log("args2 值",p3.readDouble());
                                         console.error("====================================================================------------------------------------");
                                         console.error(Thread.backtrace(this.context, Backtracer.ACCURATE).map(DebugSymbol.fromAddress).join('\\n') + '\\n');
-                                        console.error(JSON.stringify(this.context));
+                                        // console.error(JSON.stringify(this.context));
                                         // p2.writeDouble(p1.readDouble());  //锁血
 
                                         // console.log(args[3].toInt32());
@@ -35,11 +41,11 @@ function hook1(){
                         
                         },
                         onLeave: function(retval){
-                                // if(retval == 0xD3483B20 ){
-                                //         console.log("retval:",retval);
-                                //         var p4 = ptr(retval);
-                                //         console.log("retval 值",p4.readDouble());
-                                // }
+                                if(retval == 0xbd43d8a0 ){
+                                        console.log("retval:",retval);
+                                        var p4 = ptr(retval);
+                                        console.log("retval 值",p4.readDouble());
+                                }
                         }
                 });
         }
@@ -172,12 +178,206 @@ function hook4(){
                 });
         }
 }
+function hook5(){
+        // var soAddr = Module.findBaseAddress("libyoyo.so");
+        // console.log("BaseAddress:",soAddr);
+        var funAddr = Module.findExportByName("libyoyo.so", "_ZN2VM4ExecEP5CCodeP12YYObjectBaseS3_P6RValueS3_iS5_iS3_");
+        console.log("_ZN2VM4ExecEP5CCodeP12YYObjectBaseS3_P6RValueS3_iS5_iS3_ Addr:",funAddr);
+        if(funAddr != null){
+                Interceptor.attach(funAddr, {
+                        onEnter: function(args){
+                                // if(args[0] == 0xD3483B20 || args[1] == 0xD3483B20 || args[2] == 0xa28fbdac){
+                                        console.warn("_ZN2VM4ExecEP5CCodeP12YYObjectBaseS3_P6RValueS3_iS5_iS3_");
+                                        console. log("Exec args0",args[0]);
+                                        var p0 = ptr(args[0]);
+                                        console.log("args0 值",p0.readDouble());
+
+                                    
+                                        console. log("Exec args1",args[1]);
+                                        var p1 = ptr(args[1]);
+                                        console.log("args1 值",p1.readInt());
+
+                                        console. log("Exec args2",args[2]);
+                                        var p2 = ptr(args[2]);
+                                        console.log("args2 值",p2.readDouble());
+
+                                        console. log("Exec args3",args[3]);
+                                        var p3 = ptr(args[3]);
+                                        console.log("args2 值",p2.readDouble());
+
+                                        var p4 = ptr(args[2]);
+                                        console.log("args2 值",p2.readDouble());
+                                        console. log("Exec args4",args[4]);
+                                        console. log("Exec args5",args[5]);
+                                        console. log("Exec args6",args[6]);
+                                        console. log("Exec args7",args[7]);
+                                        console. log("Exec args8",args[8]);
+                                        console. log("Exec args9",args[9]);
+                                        console.log("====================================================================------------------------------------");
+                                        console.warn(Thread.backtrace(this.context, Backtracer.ACCURATE).map(DebugSymbol.fromAddress).join('\\n') + '\\n');
+                                        // console.warn(JSON.stringify(this.context));
+
+                                // }
+                        
+                        },
+                        onLeave: function(retval){
+                                // if(retval == 0xD3483B20 ){
+                                //         console.log("retval:",retval);
+                                //         var p4 = ptr(retval);
+                                //         console.log("retval 值",p4.readDouble());
+                                // }
+                        }
+                });
+        }
+}
+function hook6(){
+        // var soAddr = Module.findBaseAddress("libyoyo.so");
+        // console.log("BaseAddress:",soAddr);
+        var funAddr = Module.findExportByName("libyoyo.so", "_Z9JS_StringR6RValueP12YYObjectBaseS2_iPS_");
+        console.log("_Z9JS_StringR6RValueP12YYObjectBaseS2_iPS_ Addr:",funAddr);
+        if(funAddr != null){
+                Interceptor.attach(funAddr, {
+                        onEnter: function(args){
+                                // if(args[0] == 0xD3483B20 || args[1] == 0xD3483B20 || args[2] == 0xa28fbdac){
+                                        console.warn("_Z9JS_StringR6RValueP12YYObjectBaseS2_iPS_");
+                                        console. log("Exec args0",args[0]);
+                                        var p0 = ptr(args[0]);
+                                        console.log("args0 值",p0.readDouble());
+
+                                    
+                                        console. log("Exec args1",args[1]);
+                                        // var p1 = ptr(args[1]);
+                                        // console.log("args1 值",p1.readInt());
+
+                                        console. log("Exec args2",args[2]);
+                                        // var p2 = ptr(args[2]);
+                                        // console.log("args2 值",p2.readDouble());
+
+                                        console. log("Exec args3",args[3]);
+                                        var p3 = ptr(args[3]);
+                                        console.log("args2 值",p2.readInt());
+
+                                        var p4 = ptr(args[4]);
+                                        console.log("args2 值",p2.readDouble());
+                                        console.log("====================================================================------------------------------------");
+                                        console.warn(Thread.backtrace(this.context, Backtracer.ACCURATE).map(DebugSymbol.fromAddress).join('\\n') + '\\n');
+                                        // console.warn(JSON.stringify(this.context));
+
+                                // }
+                        
+                        },
+                        onLeave: function(retval){
+                                // if(retval == 0xD3483B20 ){
+                                //         console.log("retval:",retval);
+                                //         var p4 = ptr(retval);
+                                //         console.log("retval 值",p4.readDouble());
+                                // }
+                        }
+                });
+        }
+}
+function hook7(){
+        // var soAddr = Module.findBaseAddress("libyoyo.so");
+        // console.log("BaseAddress:",soAddr);
+        var funAddr = Module.findExportByName("libyoyo.so", "_Z24JS_String_GetOwnPropertyP12YYObjectBaseP6RValuePKc");
+        console.log("_Z24JS_String_GetOwnPropertyP12YYObjectBaseP6RValuePKc Addr:",funAddr);
+        if(funAddr != null){
+                Interceptor.attach(funAddr, {
+                        onEnter: function(args){
+                                // if(args[0] == 0xD3483B20 || args[1] == 0xD3483B20 || args[2] == 0xa28fbdac){
+                                        console.warn("_Z24JS_String_GetOwnPropertyP12YYObjectBaseP6RValuePKc");
+                                        console. log("Exec args0",args[0]);
+                                        // var p0 = ptr(args[0]);
+                                        // console.log("args0 值",p0.readDouble());
+
+                                    
+                                        console. log("Exec args1",args[1]);
+                                        var p1 = ptr(args[1]);
+                                        console.log("args1 值",p1.readDouble());
+
+                                        console. log("Exec args2",args[2]);
+                                        var p2 = ptr(args[2]);
+                                        console.log("args2 值",p2.readCString());
+
+                                        // console. log("Exec args3",args[3]);
+                                        // var p3 = ptr(args[3]);
+                                        // console.log("args2 值",p2.readInt());
+
+                                        // var p4 = ptr(args[2]);
+                                        // console.log("args2 值",p2.readDouble());
+                                        console.log("====================================================================------------------------------------");
+                                        console.warn(Thread.backtrace(this.context, Backtracer.ACCURATE).map(DebugSymbol.fromAddress).join('\\n') + '\\n');
+                                        // console.warn(JSON.stringify(this.context));
+
+                                // }
+                        
+                        },
+                        onLeave: function(retval){
+                                // if(retval == 0xD3483B20 ){
+                                //         console.log("retval:",retval);
+                                //         var p4 = ptr(retval);
+                                //         console.log("retval 值",p4.readDouble());
+                                // }
+                        }
+                });
+        }
+}
+function hook8(){
+        // var soAddr = Module.findBaseAddress("libyoyo.so");
+        // console.log("BaseAddress:",soAddr);
+        var funAddr = Module.findExportByName("libyoyo.so", "_Z23Code_Variable_Find_NamePKcii");
+        console.log("_Z23Code_Variable_Find_NamePKcii Addr:",funAddr);
+        if(funAddr != null){
+                Interceptor.attach(funAddr, {
+                        onEnter: function(args){
+                                // if(args[0] == 0xD3483B20 || args[1] == 0xD3483B20 || args[2] == 0xa28fbdac){
+                                        console.warn("_Z23Code_Variable_Find_NamePKcii");
+                                        console. log("Exec args0",args[0]);
+                                        // var p0 = ptr(args[0]);
+                                        // console.log("args0 值",p0.readDouble());
+
+                                    
+                                        console. log("Exec args1",args[1]);
+                                        var p1 = ptr(args[1]);
+                                        console.log("args1 值",p1.readDouble());
+
+                                        console. log("Exec args2",args[2]);
+                                        var p2 = ptr(args[2]);
+                                        console.log("args2 值",p2.readCString());
+
+                                        // console. log("Exec args3",args[3]);
+                                        // var p3 = ptr(args[3]);
+                                        // console.log("args2 值",p2.readInt());
+
+                                        // var p4 = ptr(args[2]);
+                                        // console.log("args2 值",p2.readDouble());
+                                        console.log("====================================================================------------------------------------");
+                                        console.warn(Thread.backtrace(this.context, Backtracer.ACCURATE).map(DebugSymbol.fromAddress).join('\\n') + '\\n');
+                                        // console.warn(JSON.stringify(this.context));
+
+                                // }
+                        
+                        },
+                        onLeave: function(retval){
+                                // if(retval == 0xD3483B20 ){
+                                //         console.log("retval:",retval);
+                                //         var p4 = ptr(retval);
+                                //         console.log("retval 值",p4.readDouble());
+                                // }
+                        }
+                });
+        }
+}
 function main(){
        	console.log("main--------begin");
 	hook1();
         // hook2();
         // hook3();
-        hook4();
+        // hook4();
+        // hook5();
+        // hook6();
+        // hook7();
+        // hook8();
 	console.log("main--------end");
 
 }
