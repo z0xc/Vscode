4686a7a1f219d6d2c41c78971014cad56ee4f3b2
z0xc <zhouxinchi1998@foxmail.com>
2021-09-17 16:56:17 +0800

    Update element.js

--- frida/this war of mine/element.js
+++ frida/this war of mine/element.js
@@ -1,23 +1,48 @@
-function call_native(a,b,c){
+function call_native(a,b){
         var so_name_str = "libAndroidGame.so";
-        var call_native_fun_str = "_ZN24KosovoInventoryContainer10AddElementERK10NameStringRK27KosovoInventoryElementEntry";
+        var call_native_fun_str = "_ZN22KosovoInventoryElement10AddElementERK27KosovoInventoryElementEntry";
         var call_native_fun_addr = Module.findExportByName(so_name_str,call_native_fun_str);
         console.log("call_native_fun_addr:",call_native_fun_addr);
-        var call_native_fun = new NativeFunction(call_native_fun_addr , 'int', ['pointer','pointer','pointer']);
+        var call_native_fun = new NativeFunction(call_native_fun_addr , 'int', ['pointer','pointer']);
         var a = ptr(a);
         var b = ptr(b);
-        var c = ptr(c);
-        call_native_fun(a,b,c);
-        call_native_fun(a,b,c);
-        call_native_fun(a,b,c);
-        call_native_fun(a,b,c);
-        call_native_fun(a,b,c);
-        call_native_fun(a,b,c);
-        call_native_fun(a,b,c);
-        call_native_fun(a,b,c);
-        call_native_fun(a,b,c);
-        call_native_fun(a,b,c);
-        call_native_fun(a,b,c);
+        // var c = ptr(c);
+        var str_data="Materials";
+        var str_data_arg = Memory.allocUtf8String(str_data);
+        console.log("str_data_arg:",str_data_arg,":",str_data_arg.readCString());
+
+        var a = Memory.alloc(8);
+        a.writePointer(str_data_arg);
+        console.log("a:",a);
+                                console.log("a.readpointer():",a.readPointer());
+                                console.log("a.readpointer().readcstring():",a.readPointer().readCString());
+                                console.log("args[1].readpointer().readcstring():",args[1].readPointer().readCString());
+                                a.sub(20);
+                                console.log(a);
+        call_native_fun(a,b);
+        call_native_fun(a,b);
+        call_native_fun(a,b);
+        call_native_fun(a,b);
+        call_native_fun(a,b);
+        call_native_fun(a,b);
+        call_native_fun(a,b);
+        call_native_fun(a,b);
+        call_native_fun(a,b);
+        call_native_fun(a,b);
+        call_native_fun(a,b);
+        call_native_fun(a,b);
+        call_native_fun(a,b);
+        call_native_fun(a,b);
+        // call_native_fun(a,b,c);
+        // call_native_fun(a,b,c);
+        // call_native_fun(a,b,c);
+        // call_native_fun(a,b,c);
+        // call_native_fun(a,b,c);
+        // call_native_fun(a,b,c);
+        // call_native_fun(a,b,c);
+        // call_native_fun(a,b,c);
+        // call_native_fun(a,b,c);
+        // call_native_fun(a,b,c);
 
 }
 function StealSpecific(){
@@ -68,6 +93,175 @@ function StealSpecific(){
         }
 
 }
+function add_element1(){
+        var so_name_str = "libAndroidGame.so";
+        var fun_name_str = "_ZN22KosovoInventoryElement10AddElementERK27KosovoInventoryElementEntry";
+        var so_addr = Module.findBaseAddress(so_name_str);
+        var fun_addr = Module.findExportByName(so_name_str,fun_name_str);
+        // var fun_addr = null;
+        console.log("so_addr:",so_addr);
+        console.log(fun_name_str," addr: ",fun_addr);
+        if(fun_addr != null){
+                Interceptor.attach(fun_addr,{
+                        onEnter:function(args){
+                                console.log("======================================================================");
+                                console.log("args[0]",args[0]);
+                                console.log("args[1]",args[1]);
+                                // console.log("args[2]",args[2]); 
+                                var call_native_fun_str = "_ZN22KosovoInventoryElement10AddElementERK27KosovoInventoryElementEntry";
+                                var call_native_fun_addr = Module.findExportByName(so_name_str,call_native_fun_str);
+                                console.log("call_native_fun_addr:",call_native_fun_addr);
+                                var call_native_fun = new NativeFunction(call_native_fun_addr , 'int', ['pointer','pointer']);
+                                // var str_data="Materials";
+                                // var str_data_arg = Memory.allocUtf8String(str_data);
+                                // console.log("str_data_arg:",str_data_arg,":",str_data_arg.readCString());
+
+                                // var a = Memory.alloc(8);
+                                // a.writePointer(str_data_arg);
+                                // console.log("a:",a);
+                                // console.log("a.readpointer():",a.readPointer());
+                                // console.log("a.readpointer().readcstring():",a.readPointer().readCString());
+                                // console.log("args[1].readpointer().readcstring():",args[1].readPointer().readCString());
+                                // args[1].writePointer(str_data_arg);
+                                // args[1].readPointer().writePointer(str_data_arg);
+                                // console.log(a);
+                                // console.log(ptr(args[1]).readpointer().readCString());
+                                // call_native_fun(args[0],args[1]);
+                                // call_native_fun(args[0],args[1]);
+                                // call_native_fun(args[0],args[1]);
+                                // call_native_fun(args[0],args[1]);
+                                // call_native_fun(args[0],args[1]);
+                                // call_native_fun(args[0],args[1]);
+                                // call_native_fun(args[0],args[1]);
+                                call_native_fun(args[0],args[1]);
+                                // call_native_fun(args[0],args[1]);
+                                // call_native_fun(args[0],args[1]);
+                                // call_native_fun(args[0],args[1]);
+                                // call_native_fun(args[0],args[1]);
+                                // call_native_fun(args[0],args[1]);
+                                // call_native_fun(args[0],args[1]);
+                                // call_native_fun(args[0],args[1]);
+                                // call_native_fun(args[0],a,args[2]);
+                                // call_native_fun(args[0],a,args[2]);
+                                // call_native_fun(args[0],a,args[2]);
+                                // call_native_fun(args[0],a,args[2]);
+                                // call_native_fun(args[0],a,args[2]);
+                                // call_native_fun(args[0],a,args[2]);
+                                // call_native_fun(args[0],a,args[2]);
+                                // call_native_fun(args[0],a,args[2]);
+                                // call_native_fun(args[0],a,args[2]);
+                                // call_native_fun(args[0],a,args[2]);
+                                // call_native_fun(args[0],a,args[2]);
+                                // call_native_fun(args[0],a,args[2]);
+                                // call_native_fun(args[0],a,args[2]);
+                                // call_native_fun(args[0],a,args[2]);
+                                // call_native_fun(args[0],a,args[2]);
+                                // // call_native_fun(args[0],str_data_arg,args[2]);
+                                // // call_native_fun(args[0],str_data_arg,args[2]);
+                                // // call_native_fun(args[0],str_data_arg,args[2]);
+                                // // call_native_fun(args[0],str_data_arg,args[2]);
+                                // // call_native_fun(args[0],str_data_arg,args[2]);
+                                // // call_native_fun(args[0],str_data_arg,args[2]);
+                                // // call_native_fun(args[0],str_data_arg,args[2]);
+                                // // call_native_fun(args[0],str_data_arg,args[2]);
+                                // // call_native_fun(args[0],str_data_arg,args[2]);
+                                // // call_native_fun(args[0],str_data_arg,args[2]);
+                                // // call_native_fun(args[0],str_data_arg,args[2]);
+                                // // call_native_fun(args[0],str_data_arg,args[2]);
+                                // // call_native_fun(args[0],str_data_arg,args[2]);
+                                // // call_native_fun(args[0],ptr(0x9fb90060),ptr(0));
+                                // // call_native_fun(args[0],ptr(0x9fb90060),ptr(0));
+                                // // call_native_fun(args[0],ptr(0x9fb90060),ptr(0));
+                                // // call_native_fun(args[0],ptr(0x9fb90060),ptr(0));
+                                // // call_native_fun(args[0],ptr(0x9fb90060),ptr(0));
+                                // // call_native_fun(args[0],ptr(0x9fb90060),ptr(0));
+                                // // call_native_fun(args[0],ptr(0x9fb90060),ptr(0));
+                                // // call_native_fun(args[0],ptr(0x9fb90060),ptr(0));
+                                // // call_native_fun(args[0],ptr(0x9fb90060),ptr(0));
+                                // // call_native_fun(args[0],ptr(0x9fb90060),ptr(0));
+                                // // call_native_fun(args[0],ptr(0x9fb90060),ptr(0));
+                                // // call_native_fun(args[0],ptr(0x9fb90060),ptr(0));
+                                // // call_native_fun(args[0],ptr(0x9fb90060),ptr(0));
+                                // // call_native_fun(args[0],ptr(0x9fb90060),ptr(0));
+                                // // call_native_fun(args[0],args[1],args[2]);
+                                // // call_native_fun(args[0],args[1],args[2]);
+                                // // call_native_fun(args[0],args[1],args[2]);
+                                // // call_native_fun(args[0],args[1],args[2]);
+                                // // call_native_fun(args[0],args[1],args[2]);
+                                // // call_native_fun(args[0],args[1],args[2]);
+                                // // call_native_fun(args[0],args[1],args[2]);
+                                // // call_native_fun(args[0],args[1],args[2]);
+                                // // call_native_fun(args[0],args[1],args[2]);
+                                // // call_native_fun(args[0],args[1],args[2]);
+                                // // call_native_fun(args[0],args[1],args[2]);
+                                // // call_native_fun(args[0],args[1],args[2]);
+                                // // call_native_fun(args[0],args[1],args[2]);
+                                // // call_native_fun(args[0],args[1],args[2]);
+                                // // call_native_fun(args[0],args[1],args[2]);
+                                // // call_native_fun(args[0],args[1],args[2]);
+                                // // call_native_fun(args[0],args[1],args[2]);
+                                // // call_native_fun(args[0],args[1],args[2]);
+                                // // call_native_fun(args[0],args[1],args[2]);
+                                // // call_native_fun(args[0],args[1],args[2]);
+                                // // call_native_fun(args[0],args[1],args[2]);
+                                // // call_native_fun(args[0],args[1],args[2]);
+                                // // call_native_fun(args[0],args[1],args[2]);
+                                // // call_native_fun(args[0],args[1],args[2]);
+                                // // call_native_fun(args[0],ptr(0x9fb90060),ptr(0));
+                                // // call_native_fun(args[0],ptr(0x9fb90060),ptr(0));
+                                // // call_native_fun(args[0],ptr(0x9fb90060),ptr(0));
+                                // // call_native_fun(args[0],ptr(0x9fb90060),ptr(0));
+                                // // call_native_fun(args[0],ptr(0x9fb90060),ptr(0));
+                                // // call_native_fun(args[0],ptr(0x9fb90060),ptr(0));
+                                // // call_native_fun(args[0],ptr(0x9fb90060),ptr(0));
+                                // // call_native_fun(args[0],ptr(0x9fb90060),ptr(0));
+                                // // call_native_fun(args[0],ptr(0x9fb90060),ptr(0));
+                                // // call_native_fun(args[0],ptr(0x9fb90060),ptr(0));
+                                // // call_native_fun(args[0],ptr(0x9fb90060),args[2]);
+                                // // call_native_fun(args[0],ptr(0x9fb90060),args[2]);
+                                // // call_native_fun(args[0],ptr(0x9fb90060),args[2]);
+                                // // call_native_fun(args[0],ptr(0x9fb90060),args[2]);
+                                // // call_native_fun(args[0],ptr(0x9fb90060),args[2]);
+                                // // call_native_fun(args[0],ptr(0x9fb90060),args[2]);
+                                // // call_native_fun(args[0],ptr(0x9fb90060),args[2]);
+                                // // call_native_fun(args[0],ptr(0x9fb90060),args[2]);
+                                // // call_native_fun(args[0],ptr(0x9fb90060),args[2]);
+                                // // call_native_fun(args[0],ptr(0x9fb90060),args[2]);
+                                // // call_native_fun(args[0],ptr(0x9fb90060),args[2]);
+                                // // call_native_fun(args[0],ptr(0x9fb90060),args[2]);
+                                // // call_native_fun(args[0],args[1],args[2]);
+                                // // call_native_fun(args[0],args[1],args[2]);
+                                // // call_native_fun(args[0],args[1],args[2]);
+                                // // call_native_fun(args[0],args[1],args[2]);
+                                // // call_native_fun(args[0],args[1],args[2]);
+                                // // call_native_fun(args[0],args[1],args[2]);
+                                // // call_native_fun(args[0],args[1],args[2]);
+                                // // call_native_fun(args[0],args[1],args[2]);
+                                // // call_native_fun(args[0],args[1],args[2]);
+                                // // call_native_fun(args[0],args[1],args[2]);
+                                // // call_native_fun(args[0],args[1],args[2]);
+                                // // this.context.pc = endaddr;
+                                // // console.log(JSON.stringify(this.context));
+                                // console.log("args[0]",args[0]);
+                                // // // console.log("args[0]",args[0].readInt());
+                                // console.log("args[1]",args[1]);
+                                // console.log("args[2]",args[2]);
+                                // console.log("args[3]",args[3]);
+                                // console.log("args[4]",args[4]);
+                                console.warn(Thread.backtrace(this.context, Backtracer.ACCURATE).map(DebugSymbol.fromAddress).join('\n') + '\n');
+                        },
+                        onLeave:function(retval){
+                                console.log("onLeave");
+                                console.log("retval:",retval);
+                                // retval.replace(0x1);
+                                console.log("======================================================================");
+                        }
+                        
+                        
+                })
+        }
+
+}
 function add_element(){
         var so_name_str = "libAndroidGame.so";
         var fun_name_str = "_ZN24KosovoInventoryContainer10AddElementERK10NameStringRK27KosovoInventoryElementEntry";
@@ -97,6 +291,8 @@ function add_element(){
                                 console.log("a.readpointer():",a.readPointer());
                                 console.log("a.readpointer().readcstring():",a.readPointer().readCString());
                                 console.log("args[1].readpointer().readcstring():",args[1].readPointer().readCString());
+                                a.sub(20);
+                                console.log(a);
                                 // args[1].writePointer(str_data_arg);
                                 // args[1].readPointer().writePointer(str_data_arg);
                                 // console.log(a);
@@ -830,8 +1026,9 @@ function inlinehook1(){
 }
 function main(){
         console.log("main begin");
-        StealSpecific();
-        add_element();
+        // StealSpecific();
+        // add_element();
+        add_element1();
         // hook1();
         // hook2()
         // hook3();
